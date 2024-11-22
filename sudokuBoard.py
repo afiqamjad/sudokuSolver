80ee999bd159fa603fa09e00b81a7834b9493039
Afiq Amjad bin Khairir <afiqk@bu.edu>
2024-11-22 15:50:39 +0900

    Started the logic for populating sudoku map

--- sudokuBoard.py
+++ sudokuBoard.py
@@ -3,9 +3,9 @@ import random as rd
 
 sudokuInitBoard = np.zeros((9,9), dtype=int)
 
-row = []
-column = []
-box = []
+row = {}
+column = {}
+box = {}
 
 def getLength() :
     return len(sudokuInitBoard)
@@ -13,9 +13,64 @@ def getLength() :
 def getNumber(a, b) :
     return sudokuInitBoard[a,b]
 
-for i in range(len(sudokuInitBoard[0])):
-    for j in range(len(sudokuInitBoard[0])):
-        if sudokuInitBoard[i,j] == 0:
-            guesser = rd.randint(0,1)
-            if guesser != 0:
-                sudokuInitBoard[i,j] = rd.randint(1,9)
+def generateNumber() :
+    return rd.randint(1,9)
+
+def checkRow(rowList, numToCheck):
+    for i in rowList:
+        if i == numToCheck:
+            return False
+    return True
+
+def checkColumn(colList, numToCheck):
+    for i in colList:
+        if i == numToCheck:
+            return False
+    return True
+
+def checkBox(boxList, numToCheck):
+    for i in boxList:
+        if i == numToCheck:
+            return False
+    return True
+
+def putNumber(number):
+    return 0
+
+def sudokuPopulator(sudokuBoard, row, col) :
+    if row > 8 or col > 8:
+        return sudokuBoard
+    
+    listNumber = []
+    number = 0
+    while listNumber != [1,2,3,4,5,6,7,8,9]:
+        number = generateNumber()
+        listNumber[number]
+        checkRow()
+        checkColumn()
+        checkBox()
+
+        if checkBox() == checkColumn() == checkRow() == True:
+            if col == 8:
+                newSudokuBoard = sudokuPopulator(newSudokuBoard, row + 1, 0)
+                if row + 1 > 8:
+                    break
+                if newSudokuBoard[row + 1, 0]== 0:
+                        continue
+            else:
+                newSudokuBoard = sudokuPopulator(newSudokuBoard, row, col + 1)
+                if col + 1 > 8:
+                    break
+                if newSudokuBoard[row, col + 1] == 0:
+                        continue
+            break
+    
+    return newSudokuBoard
+
+sudokuPopulator(sudokuInitBoard, 0, 0)
+
+## Start with one number, go down each element following each row, if valid, put in, if not valid, try with different number, if not at all valid, reset the number before and try again
+
+
+            
+
